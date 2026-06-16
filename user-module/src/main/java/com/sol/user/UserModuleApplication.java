1722e92215c69b2d74e9187bca1dda5084ea285a
jihyeon baek <qormoon@sookmyung.ac.kr>
2026-06-16 15:34:06 +0900

    feat: BaseEntity 및 JPA Auditing 설정

--- user-module/src/main/java/com/sol/user/UserModuleApplication.java
+++ user-module/src/main/java/com/sol/user/UserModuleApplication.java
@@ -2,8 +2,10 @@ package com.sol.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UserModuleApplication {
 
     public static void main(String[] args) {
