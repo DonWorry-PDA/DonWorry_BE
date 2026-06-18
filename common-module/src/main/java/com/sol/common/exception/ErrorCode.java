371a57b380dd43706c878b31cf6e5f61f0dd32d6
kim junsoo <kjs201105@naver.com>
2026-06-18 21:42:35 +0900

    feat: AUTH 에러 코드 추가

--- common-module/src/main/java/com/sol/common/exception/ErrorCode.java
+++ common-module/src/main/java/com/sol/common/exception/ErrorCode.java
@@ -17,7 +17,12 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "사용자를 찾을 수 없습니다."),
 
     // Product
-    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "PRODUCT_001", "상품을 찾을 수 없습니다.");
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "PRODUCT_001", "상품을 찾을 수 없습니다."),
+
+    // Auth
+    AUTH_001(HttpStatus.UNAUTHORIZED, "AUTH_001", "인증에 실패했습니다"),
+    AUTH_002(HttpStatus.UNAUTHORIZED, "AUTH_002", "유효하지 않은 토큰입니다"),
+    AUTH_003(HttpStatus.UNAUTHORIZED, "AUTH_003", "토큰이 만료되었습니다");
 
     private final HttpStatus httpStatus;
     private final String code;
