234a867d1c61a4455f83a8f98239731015d04a3e
jihyeon baek <qormoon@sookmyung.ac.kr>
2026-06-21 17:47:57 +0900

    feat: Survey ㅘ관련 ãErrorCode 추가

--- common-module/src/main/java/com/sol/common/exception/ErrorCode.java
+++ common-module/src/main/java/com/sol/common/exception/ErrorCode.java
@@ -25,7 +25,10 @@ public enum ErrorCode {
     // Auth
     AUTH_001(HttpStatus.UNAUTHORIZED, "AUTH_001", "인증에 실패했습니다"),
     AUTH_002(HttpStatus.UNAUTHORIZED, "AUTH_002", "유효하지 않은 토큰입니다"),
-    AUTH_003(HttpStatus.UNAUTHORIZED, "AUTH_003", "토큰이 만료되었습니다");
+    AUTH_003(HttpStatus.UNAUTHORIZED, "AUTH_003", "토큰이 만료되었습니다"),
+
+    // Survey
+    SURVEY_NOT_FOUND(HttpStatus.NOT_FOUND, "SURVEY_001", "설문 답변이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
