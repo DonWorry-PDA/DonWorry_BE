65dedbc6904e7879fc29c024e5350eec210d6acc
안혜연 <ahy020606@naver.com>
2026-06-21 17:12:42 +0900

    feat: SSE 구독 엔드포인트 추가 및 컨트롤러 경로 수정

--- user-module/src/main/java/com/sol/user/notification/controller/NotificationController.java
+++ user-module/src/main/java/com/sol/user/notification/controller/NotificationController.java
@@ -6,22 +6,30 @@ import com.sol.user.notification.service.NotificationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 
 @Tag(name = "Notification", description = "알림 API")
 @RestController
-@RequestMapping("/api/notifications")
+@RequestMapping("/api/user/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
 
     private final NotificationService notificationService;
+
+    @Operation(summary = "SSE 알림 구독", description = "실시간 알림 수신을 위한 SSE 연결")
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@RequestAttribute("userId") Long userId) {
+        return notificationService.subscribe(userId);
+    }
 
     @Operation(summary = "내 알림 목록 조회", description = "최신순으로 반환")
     @GetMapping
