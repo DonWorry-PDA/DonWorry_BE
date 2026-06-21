7848f76a02b07c59e226b36324c3f2435f55b87f
Kwon Jung <johnk200019@gmail.com>
2026-06-21 20:18:10 +0900

    refactor: 생활 안정도 API 컨트롤러 컨벤션 적용

--- user-module/src/main/java/com/sol/user/stability/controller/LifeStabilityController.java
+++ user-module/src/main/java/com/sol/user/stability/controller/LifeStabilityController.java
@@ -6,6 +6,7 @@ import com.sol.user.stability.service.LifeStabilityService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Life Stability", description = "Life stability API")
 @RestController
-@RequestMapping("/api/life-stability/me")
+@RequestMapping("/api/user/life-stability/me")
 @RequiredArgsConstructor
 public class LifeStabilityController {
 
@@ -22,13 +23,15 @@ public class LifeStabilityController {
 
     @Operation(summary = "Get latest life stability result")
     @GetMapping
-    public ApiResponse<LifeStabilityResponse> getMyLifeStability(@RequestAttribute("userId") Long userId) {
-        return ApiResponse.ok(lifeStabilityService.getLatest(userId));
+    public ResponseEntity<ApiResponse<LifeStabilityResponse>> getMyLifeStability(
+            @RequestAttribute("userId") Long userId) {
+        return ResponseEntity.ok(ApiResponse.ok(lifeStabilityService.getLatest(userId)));
     }
 
     @Operation(summary = "Recalculate life stability result")
     @PostMapping("/recalculate")
-    public ApiResponse<LifeStabilityResponse> recalculateMyLifeStability(@RequestAttribute("userId") Long userId) {
-        return ApiResponse.ok(lifeStabilityService.recalculate(userId));
+    public ResponseEntity<ApiResponse<LifeStabilityResponse>> recalculateMyLifeStability(
+            @RequestAttribute("userId") Long userId) {
+        return ResponseEntity.ok(ApiResponse.ok(lifeStabilityService.recalculate(userId)));
     }
 }
